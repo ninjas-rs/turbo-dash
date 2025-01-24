7d6653d2eacf650736a7e01c0cc8159109aebdcb
Syed Ahkam <smahkam57@gmail.com>
2025-01-25 01:40:03 +0530

    feat: add main menu bg music

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -4,12 +4,25 @@ import { EventBus } from "../event-bus";
 export class MainMenu extends Scene {
   background!: GameObjects.Image;
   player!: GameObjects.Image;
+  backgroundMusic!: Phaser.Sound.WebAudioSound;
 
   constructor() {
     super("MainMenu");
   }
 
+  init() {
+    this.events.on("shutdown", () => {
+      this.backgroundMusic.destroy();
+    });
+  }
+
   create() {
+    this.backgroundMusic = this.sound.add("main_menu_music", {
+      loop: true,
+    }) as Phaser.Sound.WebAudioSound;
+
+    this.backgroundMusic.play();
+
     EventBus.emit("current-scene-ready", this);
   }
 }
